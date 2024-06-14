b16bca64554d4d22f3250effdf62e65b9fcab769
Sameer Katija <sameerkatija@gmail.com>
2024-06-14 19:24:30 +0500

    added routing in the app, created navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import CategoriesContainer from "./components/categories-container/CategoriesContainer";
+import { Navigate, Route, Routes, redirect } from "react-router-dom";
+import HomePage from "./routes/home/HomePage";
+import NavigationBar from "./routes/navigation/NavigationBar";
 
 const App = () => {
-  const Categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <CategoriesContainer Categories={Categories} />;
+  return (
+    <Routes>
+      <Route path="/home" element={<Navigate to="/" />} />
+      <Route path="/" element={<NavigationBar />}>
+        <Route index={true} element={<HomePage />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 export default App;
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>hhhhhshajshajs shop</h1>
+    </div>
+  );
+};
---
+++ src/routes/navigation/NavigationBar.jsx
@@ -0,0 +1,23 @@
+import { Link, Outlet } from "react-router-dom";
+import "./NavigationBar.scss";
+import CrownSVG from "../../assets/crown.svg";
+function NavigationBar() {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img src={CrownSVG} />
+        </Link>
+        <div className="nav-links-container ">
+          <Link className="nav-link " to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+
+      <Outlet />
+    </>
+  );
+}
+
+export default NavigationBar;
