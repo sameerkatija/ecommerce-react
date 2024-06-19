a6ec3208851150eee4958ecbf1477a83ea39b47d
Sameer Katija <sameerkatija@gmail.com>
2024-06-19 16:05:21 +0500

    added redux persist, and redux devtools config

--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,50 @@
 import { applyMiddleware, createStore, compose } from "redux";
 import logger from "redux-logger";
 import { RootReducer } from "./root-reducer";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // const middleWares = applyMiddleware(logger);
 
-const loggerMiddleWare = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
+// const loggerMiddleWare = (store) => (next) => (action) => {
+//   if (!action.type) {
+//     return next(action);
+//   }
 
-  console.log({ "Type: ": action.type, "Payload: ": action.payload });
-  console.log("current State: ", store.getState());
+//   console.log({ "Type: ": action.type, "Payload: ": action.payload });
+//   console.log("current State: ", store.getState());
 
-  next(action);
+//   next(action);
 
-  console.log("next State: ", store.getState());
+//   console.log("next State: ", store.getState());
+// };
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
 };
 
-const middleWares = [loggerMiddleWare];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const persistedReducer = persistReducer(persistConfig, RootReducer);
+
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
+
+const composedEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 
 // export const store = createStore(RootReducer, middleWares);
 
-export const store = createStore(RootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistedStore = persistStore(store);
 // export const store = createStore(RootReducer);
